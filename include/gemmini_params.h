4c2080bdb660e85111e9a9a362b354dcf68edd22
Hansung Kim <hansung_kim@berkeley.edu>
2022-11-20 13:30:34 -0800

    Update re-generated files from changing 8b->32b

--- include/gemmini_params.h
+++ include/gemmini_params.h
@@ -8,15 +8,15 @@
 #define DIM 16
 #define ADDR_LEN 32
 #define BANK_NUM 4
-#define BANK_ROWS 4096
+#define BANK_ROWS 1024
 #define ACC_ROWS 1024
 #define MAX_BYTES 64
-#define MAX_BLOCK_LEN (MAX_BYTES/(DIM*1))
+#define MAX_BLOCK_LEN (MAX_BYTES/(DIM*4))
 #define MAX_BLOCK_LEN_ACC (MAX_BYTES/(DIM*4))
 
-typedef int8_t elem_t;
-static const elem_t elem_t_max = 127;
-static const elem_t elem_t_min = -128;
+typedef int32_t elem_t;
+static const elem_t elem_t_max = 2147483647;
+static const elem_t elem_t_min = -2147483648;
 typedef int32_t acc_t;
 typedef int64_t full_t;
 
